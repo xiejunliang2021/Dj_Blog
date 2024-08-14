6dbd725afa47af0aaddab58550d2a01734dead2f
huabenwuxin <xiejunliang2019@gmail.com>
2024-08-14 11:00:03 +0800

    修改了index的页面

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,7 +3,9 @@ from Blog.views import *
 
 
 urlpatterns = [
-    path('index/', index, name='index')
+    path('index/', index, name='index'),
+    path('return_arg/', django_return_arg, name='django_return_arg'),
+    path('django/', django_link, name='django')
 ]
 
 
--- Blog/views.py
+++ Blog/views.py
@@ -6,6 +6,19 @@ def index(request):
     return render(request, 'index.html')
 
 
+def django_link(request):
+
+    return render(request, 'django_link.html')
+
+
+def django_return_arg(request):
+
+    return render(request, 'django_return_arg.html')
+
+
+
+
+
 
 
 
