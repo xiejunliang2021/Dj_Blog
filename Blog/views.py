bda9a661353be63ec330624b005d0478fe0724d2
huabenwuxin <xiejunliang2019@gmail.com>
2024-08-19 12:50:13 +0800

    添加了add页面，用户在add页面提交数据后在数据库中创建

--- Blog/views.py
+++ Blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import *
 
 
@@ -19,14 +19,22 @@ def django_return_arg(request):
 
 def django_test_blog(request):
     info = TestBlog.objects.all()
-    dict_data = {'data': list(info)}
+    # dict_data = {'data': info}
 
-    return render(request, 'test_blog.html', dict_data)
+    return render(request, 'test_blog.html', {'data': info})
 
 
 def django_test_add(request):
+    # 当用户以GET方式访问的时候，展示给用户的是form表单，让用户来添加数据，
+    if request.method == 'GET':       # 注意这里的GET的写法，全部大写
+        return render(request, 'test_add.html')
 
-    return render(request, 'test_add.html')
+    # 当用户添加了数据以后，点击提交以后数据会以POST的方式提交回来，我们进行接收，并进行创建
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    User.objects.create(username=username, password=password)
+    #
+    return HttpResponse("添加数据成功")
 
 
 def django_test_del(request):
