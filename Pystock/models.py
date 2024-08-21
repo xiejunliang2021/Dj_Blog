0939f4ee4f6279693210fa61be2c3e2ff810c827
huabenwuxin <xiejunliang2019@gmail.com>
2024-08-21 21:07:12 +0800

    在Pystock中创建了两张表historyprice和stockcode

--- Pystock/models.py
+++ Pystock/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class StockCode(models.Model):
+    ts_code = models.CharField(max_length=16, primary_key=True, verbose_name='股票代码')
+    symbol = models.CharField(max_length=16, verbose_name='股票代码数字')
+    name = models.CharField(max_length=16, verbose_name='股票名称')
+    area = models.CharField(max_length=16,verbose_name='地区')
+    industry = models.CharField(verbose_name='行业',max_length=16)
+    list_date = models.DateField(verbose_name='上市时间')
+    market = models.CharField(verbose_name='市场类型',max_length=16)
+    list_status = models.CharField(verbose_name='上市状态',max_length=16)
+    is_hs = models.CharField(verbose_name='是否沪港通', max_length=16)
+
+
+class HistoryPrice(models.Model):
+    ts_code = models.ForeignKey(to=StockCode, to_field='ts_code', on_delete=models.CASCADE)
+    open = models.DecimalField(verbose_name='开盘价', max_digits=6, decimal_places=2)
+    close = models.DecimalField(verbose_name='收盘价', max_digits=6, decimal_places=2)
+    high = models.DecimalField(verbose_name='最高价', max_digits=6, decimal_places=2)
+    low = models.DecimalField(verbose_name='最低价', max_digits=6, decimal_places=2)
+
+
+
+
--- Pystock/tests.py
+++ Pystock/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+import tushare as ts
 
-# Create your tests here.
+pro = ts.pro_api()
+data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+print(data.head().to_string())
+
+
+
